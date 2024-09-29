49d6b535bed13bc714aa054668ae32650d7ff23f
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-09-29 10:47:55 -0300

    add routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {  BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Costs</h1>
-    </div>
-  );
+    <Router>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+
+      <Routes>
+        <Route path= "/" element={<Home/>}/> 
+        <Route path= "/Company" element={<Company/>}/> 
+        <Route path= "/Contact" element={<Contact/>}/> 
+        <Route path= "/NewProject" element={<NewProject/>}/>    
+      </Routes>
+
+      <p>Footer</p>
+
+    </Router>
+  )
 }
 
 export default App;
