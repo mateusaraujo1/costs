b43e89341942d9356c99ec2ab81c7ad2edc5151f
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-10-01 16:17:36 -0300

    add styles footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 
 import Container from './components/layout/Container';
@@ -19,9 +20,10 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route path= "/" element={<Home/>}/> 
-          <Route path= "/Company" element={<Company/>}/> 
-          <Route path= "/Contact" element={<Contact/>}/> 
-          <Route path= "/NewProject" element={<NewProject/>}/>
+          <Route path= "/company" element={<Company/>}/> 
+          <Route path= "/contact" element={<Contact/>}/> 
+          <Route path= "/projects" element={<Projects/>}/> 
+          <Route path= "/newproject" element={<NewProject/>}/>
         </Routes>
       </Container> 
 
